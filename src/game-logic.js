000708a3b599c2979c61a89c1dafea9fc18d308f
aidan073 <aidanbell073@gmail.com>
2025-06-23 12:16:55 -0400

    Refactor js to support new piece class

--- src/game-logic.js
+++ src/game-logic.js
@@ -1,6 +1,50 @@
 // Handle whenever a tile is clicked
-function handleTileClick(e, row, column){
-    alert(`You clicked tile ${row}, ${column}`);
+function handleTileClick(e, reds, blues){
+    const tile = e.currentTarget;
+    const firstChild = tile.firstElementChild;
+    if(!firstChild){
+        return;
+    }
+    if(!(firstChild.class === "piece-img")){
+        return;
+    }
+    let piece;
+    const pieceName = tile.firstElementChild.id;
+    if(reds[pieceName]){
+        piece = reds[pieceName];
+    }
+    else{
+        piece = blues[pieceName];
+    }
+    alert(`You clicked tile ${tile.dataset.row}, ${tile.dataset.column}. It contains: ${piece.type} for team ${piece.team}`);
+}
+
+// Return mover for given piece type
+function pieceTypeToMover(pieceType){
+    switch(pieceType){
+        case "num1":
+        case "num2":
+        case "num3":
+        case "num4":
+            return numericMover;
+
+        case "brute":
+            return bruteMover;
+        
+        case "ghost":
+            return ghostMover;
+    }
+}
+
+// Movers
+function numericMover(piece){
+    return;
+}
+function bruteMover(piece){
+    return;
+}
+function ghostMover(piece){
+    return;
 }
 
 // Reset board back to initial state
@@ -8,7 +52,7 @@ function resetBoard(){
     return;
 }   
 
-export { handleTileClick, resetBoard };
+export { handleTileClick, pieceTypeToMover, resetBoard};
 
 document.addEventListener('DOMContentLoaded', () => {
     const board = document.getElementById("board");
--- src/game-master.js
+++ src/game-master.js
@@ -1,10 +1,27 @@
-import { handleTileClick, resetBoard } from "./game-logic.js";
+import { handleTileClick, pieceTypeToMover, resetBoard } from "./game-logic.js";
 
 document.addEventListener('DOMContentLoaded', () => {
-    let gameState = "off";
+    class piece{
+        constructor(pieceName, currCoord){
+            this.name = pieceName;
+            this.type = pieceNameToType(pieceName, typeMap);
+            this.mover = pieceTypeToMover(this.type);
+            this.coord = currCoord;
+            if(pieceName.slice(1) === "r"){
+                this.team = "red";
+            }
+            else{
+                this.team = "blue";
+            }
+        }
+    }
+
+    const redPieces = {};
+    const bluePieces = {};
     const board = document.getElementById("board");
     const boardSize = 8; // changing this requires changing board styling currently
     const coordToTile = coord => `tile${coord[0]*boardSize+coord[1]}`;
+    
     const specialTiles = {
         "Luck": [[0, 7]],
         "Risk": [[0, 6], [1, 7]],
@@ -28,6 +45,26 @@ document.addEventListener('DOMContentLoaded', () => {
         "bNumeric4": [6, 7],
         "bGhost": [2, 7]
     }
+    const typeMap = {
+        "Brute": "brute",
+        "Numeric1": "num1",
+        "Numeric2": "num2",
+        "Numeric3": "num3",
+        "Numeric4": "num4",
+        "Ghost": "ghost"
+    };
+
+    // Convert piece name to piece type
+    function pieceNameToType(pieceName, typeMap) {
+        // Strip the team prefix
+        const baseName = pieceName.slice(1);
+        const pieceType = typeMap[baseName];
+
+        if (!pieceType) {
+            throw new Error(`Piece with name: "${baseName}" does not exist in typeMap`);
+        }
+        return pieceType;
+    }
 
     // Create tiles
     function createBoard(){
@@ -38,7 +75,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 tile.id = `tile${boardSize*row+column}`;
                 tile.dataset.row = row;
                 tile.dataset.column = column;
-                tile.addEventListener('click', (e) => handleTileClick(e, row, column));
+                tile.addEventListener('click', (e) => handleTileClick(e, redPieces, bluePieces));
                 board.appendChild(tile);
             }
         }
@@ -59,21 +96,26 @@ document.addEventListener('DOMContentLoaded', () => {
     function populateBoard(){
         // Populate reds
         for(const [k, v] of Object.entries(redSpawns)){
+            const thisPiece = new piece(k, v);
+            redPieces[k] = thisPiece;
             const currTile = document.getElementById(coordToTile(v));
-            displayPiece(k, currTile);
+            displayPiece(thisPiece, currTile);
         };
         // Populate blues
         for(const [k, v] of Object.entries(blueSpawns)){
+            const thisPiece = new piece(k, v);
+            bluePieces[k] = thisPiece;
             const currTile = document.getElementById(coordToTile(v));
-            displayPiece(k, currTile);
+            displayPiece(thisPiece, currTile);
         };
     }
 
     // Create a piece from its name, and display onto a tile
-    function displayPiece(pieceName, tile){
+    function displayPiece(piece, tile){
         const image = document.createElement("img");
-        image.src = `../imgs/pieces/${pieceName}.png`;
-        image.id = pieceName;
+        image.src = `../imgs/pieces/${piece.name}.png`;
+        image.id = piece.name;
+        image.class = "piece-img";
         tile.appendChild(image);
     }
 
@@ -90,7 +132,6 @@ document.addEventListener('DOMContentLoaded', () => {
     // When 'Play Game' is selected
     function onPlayGame(){
         setGameState("on");
-        console.log("test");
     }
 
     let playButton = document.getElementById("play-button");
